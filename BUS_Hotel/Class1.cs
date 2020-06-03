9d2f0b9cb4f89f1c9753721ddfb1823a8b6bfeea
phaptl <longphappy@gmail.com>
2020-06-03 21:05:41 +0700

    no message

---
+++ BUS_Hotel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS_Hotel
+{
+    public class Class1
+    {
+    }
+}
