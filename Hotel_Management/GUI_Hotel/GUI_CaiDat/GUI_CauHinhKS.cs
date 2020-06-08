37e1ee0ddd17e9d0d4e859fff5fa990b9245cc18
phaptl <longphappy@gmail.com>
2020-06-08 07:57:44 +0700

    GUI_CauHinh GUI_TaiKhoan

--- Hotel_Management/GUI_Hotel/GUI_CaiDat/GUI_CauHinhKS.cs
+++ Hotel_Management/GUI_Hotel/GUI_CaiDat/GUI_CauHinhKS.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Hotel_Management.GUI_CaiDat
 {
@@ -16,12 +17,71 @@ namespace Hotel_Management.GUI_CaiDat
         {
             InitializeComponent();
         }
+        private GUI_CaiDat.GUI_ListLoaiPhong guiListLP = new GUI_CaiDat.GUI_ListLoaiPhong();
+        private GUI_CaiDat.GUI_ListDichVu guiListDV = new GUI_CaiDat.GUI_ListDichVu();
+        private GUI_CaiDat.GUI_ListPhong guiListPhong = new GUI_CaiDat.GUI_ListPhong();
+        private void GUI_CauHinhKS_Load(object sender, EventArgs e)
+        {
 
-     
+
+            Thread newThread = new Thread(() =>
+            {
+                Invoke(new Action(() =>
+                {
+
+                    panelListLP.Controls.Add(guiListLP);
+                    guiListLP.Dock = DockStyle.Top;
+                    panelListDV.Controls.Add(guiListDV);
+                    guiListDV.Dock = DockStyle.Top;
+                    panelListPhong.Controls.Add(guiListPhong);
+                    guiListPhong.Dock = DockStyle.Top;
+
+                }));
+            });
+            newThread.Start();
+        }
+
 
         private void pictureBox7_Click_1(object sender, EventArgs e)
         {
             this.Close();
         }
-    }
+
+        private void btnLoaiPhong_Click(object sender, EventArgs e)
+        {
+            pageCauHinh.SetPage(0);
+        }
+
+        private void btnPhong_Click(object sender, EventArgs e)
+        {
+            pageCauHinh.SetPage(1);
+        }
+
+        private void btnDichVu_Click(object sender, EventArgs e)
+        {
+            pageCauHinh.SetPage(2);
+        }
+
+        private void btnAddLoaiPhong_Click(object sender, EventArgs e)
+        {
+            GUI_CaiDat.GUI_ListLoaiPhong guiListLP1 = new GUI_CaiDat.GUI_ListLoaiPhong();
+            panelListLP.Controls.Add(guiListLP1);
+            guiListLP1.Dock = DockStyle.Top;
+        }
+
+        private void btnAddPhong_Click(object sender, EventArgs e)
+        {
+            GUI_CaiDat.GUI_ListPhong guiListPhong = new GUI_CaiDat.GUI_ListPhong();
+            panelListPhong.Controls.Add(guiListPhong);
+            guiListPhong.Dock = DockStyle.Top;
+        }
+
+        private void btnAddDV_Click(object sender, EventArgs e)
+        {
+            GUI_CaiDat.GUI_ListDichVu guiListDV = new GUI_CaiDat.GUI_ListDichVu();
+            panelListDV.Controls.Add(guiListDV);
+            guiListDV.Dock = DockStyle.Top;
+        }
+    }       
+    
 }
---
+++ Hotel_Management/GUI_Hotel/GUI_CaiDat/GUI_ListDichVu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel_Management.GUI_CaiDat
+{
+    public partial class GUI_ListDichVu : UserControl
+    {
+        public GUI_ListDichVu()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowPanel()
+        {
+            this.Size = new Size(910, 189);
+            if (panelSubMenu.Visible == true)
+            {
+                panelSubMenu.Visible = false;
+                this.Size = new Size(910, 45);
+
+            }
+            else
+            {
+                panelSubMenu.Visible = true;
+                this.Size = new Size(910, 189);
+            }
+            
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ShowPanel();
+        }
+    }
+}
