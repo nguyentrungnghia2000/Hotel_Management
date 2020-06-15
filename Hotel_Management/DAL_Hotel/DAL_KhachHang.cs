fc7e24ae1d135806267d8905211b88dfb6269a4d
nguyentrungnghia2000 <58599798+nguyentrungnghia2000@users.noreply.github.com>
2020-06-15 20:29:09 +0700

    commit

---
+++ Hotel_Management/DAL_Hotel/DAL_KhachHang.cs
@@ -0,0 +1,18 @@
+﻿using DTO_Hotel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Hotel
+{
+    class DAL_KhachHang
+    {
+        public bool AddKhachHang(DTO_KhachHang kh)
+        {
+            string query = ""
+            return false;
+        }
+    }
+}
